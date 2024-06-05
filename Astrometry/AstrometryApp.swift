f974654b0b793c6a7474072576d9893a134a9b0a
Peter Polakovic <peter.polakovic@cloudmakers.eu>
2024-06-05 15:50:11 +0200

    Icons reverted for version 2

---
+++ Astrometry/AstrometryApp.swift
@@ -0,0 +1,18 @@
+//
+//  AstrometryApp.swift
+//  Astrometry
+//
+//  Created by Polakovic Peter on 05/06/2024.
+//  Copyright © 2024 CloudMakers, s. r. o. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct AstrometryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Astrometry/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  Astrometry
+//
+//  Created by Polakovic Peter on 05/06/2024.
+//  Copyright © 2024 CloudMakers, s. r. o. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
