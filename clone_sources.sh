5f72ea207b0e6046785e0bee635642017bbd71a3
Rumen Bogdanovski <rumaneastro@gmail.com>
2026-07-17 07:27:04 -0700

    clone sources makes astrimetry.net checkount the current branch in astrometry

--- clone_sources.sh
+++ clone_sources.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
+set -e
+
+# Remember which branch this (astrometry) repo is currently on, so we can
+# check out the matching branch in astrometry.net after cloning it.
+BRANCH=$(git rev-parse --abbrev-ref HEAD)
+
 cd ..
 git clone https://github.com/indigo-astronomy/cfitsio.git
 git clone https://github.com/indigo-astronomy/astrometry.net.git
+
+if [ "$BRANCH" != "HEAD" ]; then
+	cd astrometry.net
+	if git show-ref --verify --quiet "refs/remotes/origin/$BRANCH"; then
+		echo "Checking out branch '$BRANCH' in astrometry.net"
+		git checkout "$BRANCH"
+	else
+		echo "Branch '$BRANCH' does not exist in astrometry.net; staying on its default branch."
+	fi
+	cd ..
+fi
